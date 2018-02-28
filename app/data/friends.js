0417571dad18856f10d9dd6fcf4dcd1030704112
rickpicolli <ricardopicolli@gmail.com>
2018-02-27 22:28:34 -0800

    creating files and add some content to them

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [ {
+	"name" : "Ted Mosby",
+	"photo" : "https://vignette.wikia.nocookie.net/himym/images/a/a3/Ted_profile.jpg/revision/latest?cb=20110927011952",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "Robin Scherbatsky",
+	"photo" : "https://vignette.wikia.nocookie.net/p__/images/7/73/Cobie-smulders-joue-robin.jpg/revision/latest?cb=20121016132118&path-prefix=protagonist",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "Barney Stinson",
+	"photo" : "https://vignette.wikia.nocookie.net/icarly/images/3/37/Barney-Stinson-barney-stinson-31316015-1920-2560.jpg/revision/latest?cb=20130118171814",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "Lily Aldrin",
+	"photo" : "https://images.davidemaggio.it/pics3/2013/11/How-I-Met-Your-Mother-8-Alyson-Hannigan.jpg",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "Marshall Eriksen",
+	"photo" : "https://home.sandiego.edu/~jgnibus/marshall.jpg",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+	"name" : "Tracy McConnell (The Mother)",
+	"photo" : "https://shshkbb.files.wordpress.com/2014/04/how-i-met-your-mother-finale-08_612x380.jpg",
+	"scores" : [1,1,1,1,1,1,1,1,1,1]
+}
+
+];
+
+
+module.exports = friends;
